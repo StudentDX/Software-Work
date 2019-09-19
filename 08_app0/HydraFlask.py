3fbcb7db3c3d336813922219ce160c56ffd9975e
David Xiedeng <dxiedeng00@stuy.edu>
2019-09-18 23:06:10 -0400

    got routes to work very simply with url

--- 08_app0/HydraFlask.py
+++ 08_app0/HydraFlask.py
@@ -1,18 +1,23 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hell():
-    print(__name__)
-    app.route("/static/Page1.html")
-    return "test"
+#website used to figure routes out 
+#https://www.rithmschool.com/courses/flask-fundamentals/routing-with-flask
 
-@app.route("/static/Page1.html")
-def hello():
+@app.route("/")
+def test1():
     print(__name__)
     return "test 1"
 
+@app.route("/test2") #naming scheme is done with /<funct name>
+def test2():
+    print(__name__)
+    return "test 2"
 
+@app.route("/test3") #to use route, type in the url the route tag
+def test3():
+    print(__name__)
+    return "test 3"
 
 if __name__ == "__main__":
     app.debug = True
