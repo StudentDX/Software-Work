ca761d8519d5a2303c5ba4e08c6a85eb3f0d7852
David Xiedeng <dxiedeng00@stuy.edu>
2019-09-19 08:48:31 -0400

    work for 2019-09-19

---
+++ flaskApp/flaskApp.py
@@ -0,0 +1,19 @@
+#Clyde "Thluffy" Sinclair
+#SoftDev1 pd0
+#demo -- My First Flask App
+#2019-09-17t
+
+from flask import Flask
+app = Flask(__name__) #create instance of class Flask
+
+@app.route("/")
+ #assign following fxn to run when root route requested
+def hello_world():
+    print(__name__) #where will this go?
+    return "No hablo queso!"
+
+coll = {0,1,2,3}
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
