b8a1d5410d769f906f64bf802d128480ca34955e
David Xiedeng <dxiedeng00@stuy.edu>
2019-10-02 19:55:03 -0400

    added main page for login

--- 15_login/app.py
+++ 15_login/app.py
@@ -1,4 +1,26 @@
 #David Xiedeng
 #SoftDev1 pd 1
 #K15 -- Do I Know You?
-#2019-10-02 
+#2019-10-02
+
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+
+@app.route("/")
+def login(): 
+  return render_template("login.html")
+  
+@app.route("/auth")
+def loginAccepted():
+  """
+  print("request:", request)
+  print("request.args:", request.args)
+  print("request.method:", request.method)
+  print("request.headers:", request.headers)
+  """
+  if request.args["username"] == "cleancoal" and request.args["password"] == "co2":
+    return "test"
+  
+if __name__ == "__main__":
+  app.debug = True
+  app.run() 
