567a98db0bc13df55fa95b33c98ce53c2e7db46a
David Xiedeng <dxiedeng00@stuy.edu>
2019-10-03 22:40:49 -0400

    tried adding sessions; stopped to understand secret keys

--- 15_login/app.py
+++ 15_login/app.py
@@ -3,8 +3,12 @@
 #K15 -- Do I Know You?
 #2019-10-02
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
+
+#hardcoded username and password
+username = "cleancoal"
+password = "co2"
 
 @app.route("/")
 def login(): 
@@ -17,14 +21,29 @@ def loginAccepted():
   print("request.args:", request.args)
   print("request.method:", request.method)
   print("request.headers:", request.headers)
-  """
-  username = request.args["username"]
   print ("User:", username)
-  if username == "cleancoal" and request.args["password"] == "co2":
+  """
+  session["username"] = request.args["username"]
+  session["password"] = request.args["password"]
+  if username == session["username"] and password == session["password"]:
     return render_template("profile.html", 
-      User = username)
+      User = session[username])
   else:
-    return redirect("/static/error.html")
+    return redirect("/error")
+
+@app.route("/error")
+def badLogin():
+  error = ""
+  # checks for mistake in the entered information
+  #print(username)
+  #print(request.args)
+  if username != session["username"] and password != session["password"]:
+    error = "username and password"
+  elif username != session["username"]:
+    error = "username"
+  else:
+    error = "password"
+  return render_template("error.html", mistake = error)
     
 if __name__ == "__main__":
   app.debug = True
