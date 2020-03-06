6f71e6245517a5b524aa61dc64335bc92139e918
David - Mongo Virtual Box <dxiedeng00@stuy.edu>
2020-03-06 01:09:45 -0500

    added some documentation from Mongo site; rearranged code slightly

--- 09_mongo/app.py
+++ 09_mongo/app.py
@@ -1,15 +1,28 @@
 #David Xiedeng
 #SoftDev1 pd 1
 #K09 -- Yummy Mongo Py
-#2020-02-28 
+#2020-02-28
 
 from pymongo import MongoClient
 import json
 
+#creating MongoClient
+#uses default perameters of local host
+client = MongoClient()
+
+#creates Mongo database
+#does not create or save at the start
+#saves when first document is inserted
+db = client.newBInfo
+
+#creates Mongo collection
+#does not create or save at the start
+#saves when first document is inserted
+collection = db.addresses
+
 def convertJSONtoMongoDB(filename):
     f = open(filename, "r")
     data = json.load(f)
-    for el in data:
-        print (el)
+    print (data[1])
 
-convertJSONtoMongoDB("primer-dataset.json")
+#convertJSONtoMongoDB("primer-dataset.json")
