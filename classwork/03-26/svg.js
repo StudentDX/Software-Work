ff871c80e2fa4ebf3e3467f4a1acd6843441db70
David - Mongo Virtual Box <dxiedeng00@stuy.edu>
2020-03-26 09:11:48 -0400

    copied code from Mandy-Physics in Slack learnination original code seemed to be identical, character for character would not function at all by comparison, no change help

--- classwork/03-26/svg.js
+++ classwork/03-26/svg.js
@@ -1,13 +1,8 @@
-
-var pic = document.getElementByID("vimage"):
-
-var c = document.createElementNX(
-  "http://www.w3.org/2000/svg", "circle"
-);
-c.setAttribute("cx", 0);
-c.setAttribute( "cy", 0);
-c.setAttribute("r", "100");
-c.setAttribute( "fill", "red");
-c.setAttribute( "stroke", "black");
-
+var pic= document.getElementById("vimage");
+var c= document.createElementNS("http://www.w3.org/2000/svg","circle");
+c.setAttribute("cx",0);
+c.setAttribute("cy",0);
+c.setAttribute("r",100);
+c.setAttribute("fill","red");
+c.setAttribute("stroke","black");
 pic.appendChild(c);
