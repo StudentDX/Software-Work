c6fc30bab430dc0e0debbc82878321bcd191beb9
David - Mongo Virtual Box <dxiedeng00@stuy.edu>
2020-04-19 22:41:07 -0400

    Got Started

---
+++ 18_d3/app.py
@@ -0,0 +1,6 @@
+'''
+David Xiedeng
+Softdev pd 1
+K18 -- Come Up For Air
+2020-04-19
+'''
---
+++ 18_d3/script.js
@@ -0,0 +1,10 @@
+/*
+David Xiedeng
+Softdev pd 1
+K18 -- Come Up For Air
+2020-04-19
+*/
+
+//to open file
+// in this case a json
+attachment = FileAttachment("meteorites.json")
