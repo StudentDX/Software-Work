5eb2bd9ea4f9463733d940846e80d587e3982c2e
David - Mongo Virtual Box <dxiedeng00@stuy.edu>
2020-04-20 22:10:18 -0400

    Have passthrough of info to js working

--- 18_d3/app.py
+++ 18_d3/app.py
@@ -5,7 +5,7 @@ K18 -- Come Up For Air
 2020-04-19
 '''
 
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template
 import json
 
 app = Flask(__name__)
@@ -17,8 +17,11 @@ def readJSON(filename):
 
 @app.route("/")
 def start():
-    print (type(readJSON("meteorites.json")))
-    return redirect("/static/index.html")
+    data = readJSON("static/meteorites.json")
+    #print (data)
+    return render_template(
+        "index.html",
+        info = data)
 
 if __name__ == "__main__":
     app.debug = True
--- 18_d3/script.js
+++ 18_d3/script.js
@@ -4,8 +4,3 @@ Softdev pd 1
 K18 -- Come Up For Air
 2020-04-19
 */
-
-//to open file
-// in this case a json
-data = d3.json("./meteorites.json", callback); 
-);
