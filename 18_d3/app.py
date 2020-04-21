a8b0605af2609f4cd079513e4bc191b1b7e3887f
David - Mongo Virtual Box <dxiedeng00@stuy.edu>
2020-04-20 21:11:36 -0400

    Have working flask to move to index.html - moved index.html and meteorites.json to static dir

--- 18_d3/app.py
+++ 18_d3/app.py
@@ -5,12 +5,21 @@ K18 -- Come Up For Air
 2020-04-19
 '''
 
+from flask import Flask, render_template, redirect
 import json
+
+app = Flask(__name__)
 
 def readJSON(filename):
     with open(filename, "r") as file:
         data = json.load(file)
-        for el in data:
-            print el
+        return data
 
-readJSON("meteorites.json")
+@app.route("/")
+def start():
+    print (type(readJSON("meteorites.json")))
+    return redirect("/static/index.html")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
