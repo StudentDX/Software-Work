531ba775596141117aa8b7d2ba526c3c8f9ccd8f
David - Mongo Virtual Box <dxiedeng00@stuy.edu>
2020-04-21 00:11:13 -0400

    Found lowest year to select as 1880

--- 18_d3/static/script.js
+++ 18_d3/static/script.js
@@ -7,8 +7,19 @@ K18 -- Come Up For Air
 
 //passing info from Flask to js
 //indirectly uses a meta tag
-var fromFlask = document.getElementById('server-info')
+var fromFlask = document.getElementById('server-info');
 // java info
-var data = fromFlask.dataset.server
-data = JSON.parse(data)
-fromFlask.remove()
+var data = fromFlask.dataset.server;
+data = JSON.parse(data);
+fromFlask.remove();
+
+//lowest year in selection is 1880
+var currentYear = 1880;
+
+function filterByYear(yearstring) {
+  filteredData = [];
+  for (i = 0; i < data.length; i += 1) {
+    filteredData.push(data[i]);
+  }
+  return filteredData;
+}
